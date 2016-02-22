adfce8b01ceeb56ef4a6204b5b081bfe139fc807
Hadrien Croubois <hadrien.croubois@gmail.com>
2016-02-22 10:55:50 +0100

    debug admin javascript policy

--- admin/index.php
+++ admin/index.php
@@ -27,7 +27,7 @@
 		<!-- L i b   -   J q u e r y -->
 		<!-- <script type="text/javascript" src="../ressources/libs/jquery-2.1.3.js"></script> -->
 		<!-- <script type="text/javascript" src="../ressources/libs/jquery-ui.min.js"></script> -->
-		<script async type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 		<script async type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 
 
