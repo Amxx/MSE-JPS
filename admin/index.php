76e0510cf7355f902a8319041f684cdf8b54702f
Hadrien Croubois <hadrien.croubois@gmail.com>
2016-04-27 14:55:26 +0200

    ressources //

--- admin/index.php
+++ admin/index.php
@@ -41,7 +41,7 @@
 		<!--
 		<link rel="stylesheet" href="ressources/font/OpenSans.css" type="text/css"/>
 		-->
-		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+		<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 
 		<!-- M S E J P S   -   A d m i n i s t r a t i o n -->
 		<script type="text/javascript" src="../ressources/script/admin-min.js"></script>
